88cbd0df6ec40c315c42d01c1959faf2c5535454
nickluong97@outlook.com <nickluong97@outlook.com>
2021-01-16 12:44:49 -0800

    Make glow

--- form.py
+++ form.py
@@ -6,5 +6,5 @@ from wtforms.validators import InputRequired
 class WordSearchForm(FlaskForm):
 
     steam_id = StringField('SteamID', validators=[InputRequired()],
-                render_kw={"placeholder": "Input your steam ID:"})
+                render_kw={"placeholder": "Input your steam ID"})
     submit = SubmitField('Go')
