667a2e9e2ea4e43ab718cccc7524368ebe6eea9e
nickluong97@outlook.com <nickluong97@outlook.com>
2021-01-16 12:54:40 -0800

    Make glow

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
 from form import WordSearchForm
+import os
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/Hack-the-North-2021')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env.key'))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz'
--- get_total_playtime.py
+++ get_total_playtime.py
@@ -2,10 +2,11 @@ import json
 from urllib.request import urlopen
 
 
-def get_total_hours():
+def get_total_hours(steam_id):
     key = input("Please enter your steam api key: ")
-    id = input("Please enter your steam id: ")
-    total_hours = json.load(urlopen(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={id}&format=json"))
+    # id = input("Please enter your steam id: ")
+    total_hours = json.load(urlopen(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}"
+                                    f"&steamid={steam_id}&format=json"))
     return total_hours["response"]["games"]
 
 
