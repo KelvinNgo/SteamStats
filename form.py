29e2d309cd13d69fa8959ee4127b63aaf7b4adc3
nickluong97@outlook.com <nickluong97@outlook.com>
2021-01-16 11:50:46 -0800

    We changed ideas. Removed unused code. Added form and changed base.html

---
+++ form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
+
+
+class WordSearchForm(FlaskForm):
+
+    steam_id = StringField('Search', validators=[InputRequired()],
+                render_kw={"placeholder": "Input your steam ID:"})
+    submit = SubmitField('Go')
