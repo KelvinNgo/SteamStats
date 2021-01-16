579bd1dfc3b4e8f403fcf81a73c41b86d5080f48
nickluong97@outlook.com <nickluong97@outlook.com>
2021-01-16 10:55:04 -0800

    Messing around with steam auth.

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
 from os.path import dirname, join
-from flask import Flask, g, redirect, session, json
+from flask import Flask, g, redirect, session, json, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_openid import OpenID
 import urllib
 from urllib.request import urlopen
 import re
 
+
 app = Flask(__name__)
 app.config.update(
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///flask-openid.db',
-    SECRET_KEY = 'STEAM API KEY',
-    DEBUG = True,
-    PORT = 5000
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///flask-openid.db',
+    SECRET_KEY='STEAM API KEY',
+    DEBUG=True,
+    PORT=5000
 )
 
+
 oid = OpenID(app, join(dirname(__file__), 'openid_store'))
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    steam_id = db.Column(db.String(40))
-    nickname = db.Column(db.String(80))
-
-    @staticmethod
-    def get_or_create(steam_id):
-        rv = User.query.filter_by(steam_id=steam_id).first()
-        if rv is None:
-            rv = User()
-            rv.steam_id = steam_id
-            dbs.session.add(rv)
-        return rv
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     steam_id = db.Column(db.String(40))
+#     nickname = db.Column(db.String(80))
+#
+#     @staticmethod
+#     def get_or_create(steam_id):
+#         rv = User.query.filter_by(steam_id=steam_id).first()
+#         if rv is None:
+#             rv = User()
+#             rv.steam_id = steam_id
+#             dbs.session.add(rv)
+#         return rv
 
 
 _steam_id_re = re.compile('steamcommunity.com/openid/id/(.*?)$')
 
+@oid.after_login
 def get_steam_userinfo(steam_id):
     options = {
         'key': app.secret_key,
@@ -44,12 +47,15 @@ def get_steam_userinfo(steam_id):
           'GetPlayerSummaries/v0001/?%s' % urllib.urlencode(options)
     rv = json.load(urlopen(url))
     return rv['response']['players']['player'][0] or {}
+    return render_template("base.html", rv=rv['response']['players']['player'][0])
+
 
 @app.before_request
 def before_request():
     g.user = None
     if 'user_id' in session:
         g.user = User.query.filter_by(id=session['user_id']).first()
+
 
 @app.route("/login")
 @oid.loginhandler
@@ -59,20 +65,36 @@ def login():
     else:
         return oid.try_login("http://steamcommunity.com/openid")
 
+
 @oid.after_login
 def new_user(resp):
     match = _steam_id_re.search(resp.identity_url)
-    g.user = User.get_or_create(match.group(1))
-    steamdata = get_steam_userinfo(g.user.steam_id)
-    g.user.nickname = steamdata['personaname']
-    db.session.commit()
-    session['user_id'] = g.user.id
-    return redirect(oid.get_next_url())
+    # g.user = User.get_or_create(match.group(1))
+    # steamdata = get_steam_userinfo(g.user.steam_id)
+    # g.user.nickname = steamdata['personaname']
+    # db.session.commit()
+    # session['user_id'] = g.user.id
+    # return redirect(oid.get_next_url())
+
+
+    check = _steam_id_re.findall(resp.identity_url)[0]
+    options = {
+        'key': app.secret_key,
+        'steamids': check #steam_id
+    }
+    url = 'http://api.steampowered.com/ISteamUser/' \
+          'GetPlayerSummaries/v0001/?%s' % urllib.urlencode(options)
+    rv = json.load(urlopen(url))
+    # return rv['response']['players']['player'][0] or {}
+
+    return render_template("base.html", id=rv['response']['players']['player'][0])
+
 
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
     return redirect(oid.get_next_url())
+
 
 @app.route('/')
 def hello():
@@ -80,6 +102,7 @@ def hello():
         return "Hi user %s with steam id %s <a href='/logout'>logout</a>" % (g.user.nickname, g.user.steam_id)
     else:
         return "You are not logged in <a href='/login'>login</a>"
+
 
 if __name__ == "__main__":
     app.run()
