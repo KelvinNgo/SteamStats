a08f7e328a29a8838a9541317e3a6e5abc196e5e
Kelvin Ngo <kelvinngosos@gmail.com>
2021-01-16 11:59:34 -0800

    here you go buddy guy

--- get_total_playtime.py
+++ get_total_playtime.py
@@ -3,7 +3,9 @@ from urllib.request import urlopen
 
 
 def get_total_hours():
-    total_hours = json.load(urlopen("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=33A26580F04F1550F02B0B4A4B0E8DD1&steamid=76561198159557689&format=json"))
+    key = input("Please enter your steam api key: ")
+    id = input("Please enter your steam id: ")
+    total_hours = json.load(urlopen(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={id}&format=json"))
     return total_hours["response"]["games"]
 
 
