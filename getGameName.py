ad38caffc2015951b477ef143f973658420ade9f
Kelvin Ngo <kelvinngosos@gmail.com>
2021-01-16 13:31:30 -0800

    here you go buddy guy!

--- getGameName.py
+++ getGameName.py
@@ -2,9 +2,7 @@ import json
 from urllib.request import urlopen
 
 
-def get_game_name():
-    key = input("Please enter your steam api key: ")
-    game_id = input("Please enter your game id: ")
+def get_game_name(game_id):
     app_list = load_all_games()
     game = list(filter(lambda game_container: game_container['appid'] == int(game_id), app_list))
     try:
@@ -18,6 +16,3 @@ def load_all_games():
     app_container = json.load(urlopen("https://api.steampowered.com/ISteamApps/GetAppList/v2/"))
     app_list = app_container["applist"]["apps"]
     return app_list
-
-
-print(get_game_name())
--- get_total_playtime.py
+++ get_total_playtime.py
@@ -2,7 +2,7 @@ import json
 from urllib.request import urlopen
 
 
-def get_total_hours():
+def get_total_hours() -> list:
     key = input("Please enter your steam api key: ")
     id = input("Please enter your steam id: ")
     total_hours = json.load(urlopen(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={id}&format=json"))
